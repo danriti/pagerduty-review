5b7f7e3a67025df967381fb69d389a28c5e206c8
Dan Riti <dmriti@gmail.com>
2016-05-16 22:13:48 +0000

    Fix css build issue with webpack.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -19,13 +19,14 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader') },
+      //{ test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') },
       { include: /\.json$/, loaders: ["json-loader"] }
     ]
   },
 
   resolve: {
-    extensions: ['', '.js', '.jsx', '.css']
+    extensions: ['', '.js', '.jsx', '.json', '.css']
   },
 
   postcss: [
